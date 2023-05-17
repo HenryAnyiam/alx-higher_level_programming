e70cfb4e6cd5289d7bcfeda67e3436aacf30096f
HenryAnyiam <anyiamhenry7@gmail.com>
2023-05-17 06:09:11 -0700

    different sort method

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if type(a_dictionary) == dict:
+        if len(a_dictionary) == 0:
+            return None
         a = list(a_dictionary.items())
         a.sort(key=lambda n: n[1])
         return a[-1][0]
