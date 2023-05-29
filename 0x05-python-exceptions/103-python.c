b027feedd6d9228f402b244fd8e9ed65ab817d62
HenryAnyiam <anyiamhenry7@gmail.com>
2023-05-29 16:04:29 -0700

    floating point precision

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -80,7 +80,7 @@ void print_python_list(PyObject *p)
 void print_python_float(PyObject *p)
 {
 	char buf[1024];
-	int i, a = 0;
+	int i;
 
 	fflush(stdout);
 	printf("[.] float object info\n");
@@ -98,14 +98,6 @@ void print_python_float(PyObject *p)
 		{
 			printf("%c", buf[i]);
 			i++;
-			if (buf[i] == '0' && buf[i + 1] != '\0')
-				a = i;
-			while (buf[i] == '0' && buf[i + 1] != '\0')
-				i++;
-			if ((buf[i] >= '1' && buf[i] <= '9') && a != 0)
-				for ( ; a < i; a++)
-					printf("%c", buf[a]);
-			a = 0;
 		}
 		printf("\n");
 	}
