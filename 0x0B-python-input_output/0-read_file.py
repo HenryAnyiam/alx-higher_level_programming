6421c31faabdd7aee24fa818dae8dc22655666af
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-13 16:49:27 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
 
+"""0-read_file module
+
+func:
+    read_file
+
+"""
+
+
 def read_file(filename=""):
+    """read from a file and writes to stdout
+
+    Args:
+        filename - name of file
+
+    """
+
     with open(filename, encoding="UTF-8") as MyFile:
         print(MyFile.read(), end="")
