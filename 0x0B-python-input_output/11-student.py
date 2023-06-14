814a19fcc2224826edfbb57ad0d383bd0f252ef8
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-13 21:44:22 -0700

    added control flow

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -33,6 +33,9 @@ class Student:
     def reload_from_json(self, json):
         """replaces attributes of student instance"""
 
-        self.first_name = json["first_name"]
-        self.last_name = json["last_name"]
-        self.age = json["age"]
+        if "first_name" in json:
+            self.first_name = json["first_name"]
+        if "last_name" in json:
+            self.last_name = json["last_name"]
+        if "age" in json:
+            self.age = json["age"]
