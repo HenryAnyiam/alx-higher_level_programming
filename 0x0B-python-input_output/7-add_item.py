c263aa6aeb110a8a09965da3388c07b6fa4b37a3
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-13 19:10:12 -0700

    adds argument to a python list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+"""7-add_item module
+
+Reads args, appends to a list and serialize
+
+"""
+
+import sys
+import json
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+items  = []
+
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    pass
+else:
+    for i in obj:
+        items.append(i)
+print(items)
+args = sys.argv
+if len(args) > 1:
+    for i in range(1, len(args)):
+        items.append(args[i])
+print(items)
+save_to_json_file(items, "add_item.json")
