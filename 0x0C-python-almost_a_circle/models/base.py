932747c08800bfd2649407812bf4cb159ab78bb3
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-22 14:43:28 -0700

    fix update method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,7 +57,7 @@ class Base:
     def create(cls, **dictionary):
         """creates a new class instance with given **kwargs"""
 
-        dummy = cls(3, 1, 1)
+        dummy = cls(3, 1)
         dummy.update(**dictionary)
         return dummy
 
