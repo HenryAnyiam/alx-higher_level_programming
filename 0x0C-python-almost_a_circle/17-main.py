e0ef8ac8de237bc5ffbcf3842a4a678994d8322b
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-22 14:56:18 -0700

    fixed update method

--- 0x0C-python-almost_a_circle/17-main.py
+++ 0x0C-python-almost_a_circle/17-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ 17-main """
 from models.rectangle import Rectangle
-
+from models.square import Square
 if __name__ == "__main__":
 
     r1 = Rectangle(3, 5, 1)
--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,9 +57,10 @@ class Base:
     def create(cls, **dictionary):
         """creates a new class instance with given **kwargs"""
 
-        dummy = cls(3, 1)
         if (cls.__name__ == "Square"):
             dummy = cls(3)
+        else:
+            dummy = cls(3, 1)
         dummy.update(**dictionary)
         return dummy
 
