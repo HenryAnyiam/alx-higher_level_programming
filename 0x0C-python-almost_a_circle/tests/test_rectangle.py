f51e8b2b2f1ea944744fa02d41cff7fb114bbd72
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-23 19:12:14 -0700

    building tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -2,11 +2,63 @@
 """Unittest for rectangle class
 """
 import unittest
-from models.base import Base
+import sys
+from io import StringIO
 from models.rectangle import Rectangle
-from models.square import Square
+
 
 class TestRectangle(unittest.TestCase):
+    """Test class for class Rectangle
 
-    def test_id(self):
-        pass
+    Args:
+        unittest.TestCase: class being inherited from
+
+    """
+
+    def capt(self, method, *args):
+        """capture printed output by redirecting it"""
+
+        cap = StringIO()
+        sys.stdout = cap
+        method(*args)
+        text = cap.getvalue()
+        sys.stdout = sys.__stdout__
+        return text
+
+    def test_instance(self):
+        """Test for Rectangle instance"""
+
+        self.assertTrue(Rectangle(1, 2))
+        self.assertTrue(Rectangle(1, 2, 3))
+        self.assertTrue(Rectangle(1, 2, 3, 4))
+        self.assertTrue(Rectangle(1, 2, 3, 4, 5))
+
+    def test_instance_Error(self):
+        """Test for correct Error raise"""
+
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+            Rectangle(1, "2")
+            Rectangle(1, 2, "3")
+            Rectangle(1, 2, 3, "4")
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+            Rectangle(1, -2)
+            Rectangle(0, 2)
+            Rectangle(1, 0)
+            Rectangle(1, 2, -3)
+            Rectangle(1, 2, 3, -4)
+
+    def test_methods(self):
+        """Test for various rectangle methods"""
+
+        r1 = Rectangle(4, 6, 2, 1, 2)
+        r2 = Rectangle(2, 2)
+        r3 = Rectangle(3, 2, 1)
+        r4 = Rectangle(3, 2, 1, 1)
+        self.assertEqual(r1.area(), 24)
+        self.assertEqual(self.capt(print, r1), '[Rectangle] (2) 2/1 - 4/6\n')
+        self.assertEqual(self.capt(r2.display), '##\n##\n')
+        self.assertEqual(self.capt(r3.display), ' ###\n ###\n')
+        self.assertEqual(self.capt(r4.display), '\n ###\n ###\n')
