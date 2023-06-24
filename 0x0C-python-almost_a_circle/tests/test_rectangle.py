2ef5f9741a01ffaf7a397678f9ffc7b403d75231
HenryAnyiam <anyiamhenry7@gmail.com>
2023-06-23 19:25:19 -0700

    building tests

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -36,18 +36,26 @@ class TestRectangle(unittest.TestCase):
     def test_instance_Error(self):
         """Test for correct Error raise"""
 
-        with self.assertRaises(TypeError):
+        with self.assertRaisesRegex(TypeError, 'width must be an integer'):
             Rectangle("1", 2)
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
             Rectangle(1, "2")
+        with self.assertRaisesRegex(TypeError, 'x must be an integer'):
             Rectangle(1, 2, "3")
+        with self.assertRaisesRegex(TypeError, 'y must be an integer'):
             Rectangle(1, 2, 3, "4")
 
-        with self.assertRaises(ValueError):
+        with self.assertRaisesRegex(ValueError, 'width must be > 0'):
             Rectangle(-1, 2)
+        with self.assertRaisesRegex(ValueError, 'height must be > 0'):
             Rectangle(1, -2)
+        with self.assertRaisesRegex(ValueError, 'width must be > 0'):
             Rectangle(0, 2)
+        with self.assertRaisesRegex(ValueError, 'height must be > 0'):
             Rectangle(1, 0)
+        with self.assertRaisesRegex(ValueError, 'x must be >= 0'):
             Rectangle(1, 2, -3)
+        with self.assertRaisesRegex(ValueError, 'y must be >= 0'):
             Rectangle(1, 2, 3, -4)
 
     def test_methods(self):
