b16a80f3db998658064c6c8a7bd43603d5c94a4a
HenryAnyiam <anyiamhenry7@gmail.com>
2023-07-19 16:16:17 -0700

    handling RDBMS

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@
 SELECT g.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres AS g INNER JOIN tv_show_genres AS t
 ON g.id = t.genre_id
- ORDER BY number_of_shows DESC;
+GROUP BY g.name  ORDER BY number_of_shows DESC;
