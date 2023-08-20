cc9d0fb251b63d5a4733775ef6ef38bd3410683f
HenryAnyiam <anyiamhenry7@gmail.com>
2023-08-19 17:47:34 -0700

    using sqlalchemy

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -17,4 +17,4 @@ if __name__ == "__main__":
     for state in Query:
         print(f"{state.id}: {state.name}")
         for city in state.cities:
-            print(f"\t{city.id}: {city.name}")
+            print(f"    {city.id}: {city.name}")
