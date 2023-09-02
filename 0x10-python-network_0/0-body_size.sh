8e309b6c5ed572b3469b2eeded690f73de47e861
HenryAnyiam <anyiamhenry7@gmail.com>
2023-09-02 05:31:39 -0700

    using curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #takes a URL, sends a request to it and displays the size
 curl -s -o output.html -w "%{size_download}" $1;
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 #takes a URL, sends a request to it and displays the size
 curl -s -L $1;
